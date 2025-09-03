9dfa1d7901cdffb5bcd448d4ec105b603dfd43c3
genericvillager <seanjack1014@gmail.com>
2025-09-03 01:04:56 +0100

    Fixing issues

---
+++ Forklift UnCertified/Assets/Scripting/Example Script/Box.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Box : MonoBehaviour
+{
+    public UnityEvent OnWeightChange;
+
+    private List<Box> boxesAbove;
+    private List<Box> boxesBelow;
+
+    [SerializeField] private float weight = 1;
+
+    private void Start()
+    {
+        boxesAbove = new();
+        boxesBelow = new();
+        OnWeightChange = new();
+        OnWeightChange.AddListener(InvokeOnWeightChangeRecursive);
+    }
+
+    public float Weight
+    {
+        get
+        {
+            if (boxesAbove.Count == 0)
+                return weight / Mathf.Max(boxesBelow.Count, 1);
+            else
+            {
+                float runningWeight = 0f;
+                foreach (Box above in boxesAbove)
+                {
+                    runningWeight += above.Weight;
+                }
+                return (runningWeight + weight) / Mathf.Max(boxesBelow.Count, 1);
+            }
+        }
+    }
+
+    public bool playerPushable = true;
+
+    [Range(0f, 1f)] public float playerCoefficientOfRestitution = 0.2f;
+
+    public virtual void PickUp()
+    {
+
+    }
+
+    public virtual void PutDown()
+    {
+
+    }
+
+    public virtual void Lift()
+    {
+
+    }
+
+    public virtual void Lower()
+    {
+
+    }
+
+    public void InvokeOnWeightChangeRecursive()
+    {
+        foreach(Box box in boxesBelow)
+        {
+            box.OnWeightChange.Invoke();
+            box.InvokeOnWeightChangeRecursive();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.body != null && collision.body.gameObject.TryGetComponent<Box>(out Box box))
+        {
+            if (collision.body.transform.position.y > transform.position.y + 0.4f)
+            {
+                boxesAbove.Add(box);
+            }
+            else if (collision.body.transform.position.y < transform.position.y - 0.4f)
+            {
+                boxesBelow.Add(box);
+            }
+            OnWeightChange.Invoke();
+            box.InvokeOnWeightChangeRecursive();
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.body != null && collision.body.gameObject.TryGetComponent<Box>(out Box box))
+        {
+            boxesAbove.Remove(box);
+            boxesBelow.Remove(box);
+            OnWeightChange.Invoke();
+            box.InvokeOnWeightChangeRecursive();
+        }
+    }
+}
